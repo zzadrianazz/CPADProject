98e19efb406f39919cf618791f8ffc59cbbc4b32
Adriana Christ <G00419031@atu.ie>
2025-11-05 16:39:44 +0000

    Add project files.

---
+++ CPADProject/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CPADProject")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CPADProject.Pages")]
---
+++ CPADProject/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CPADProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
